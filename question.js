ebabbfcf13c71e6d5b4dff7b596b40458cc4671f
littlepann <45886128+littlepann@users.noreply.github.com>
2018-12-14 19:25:00 -0500

    update answers

--- question.js
+++ question.js
@@ -1,15 +1,14 @@
 //questions and answer choices
-let qList1 = ['ayyyy'];
-let aList1 = ['ayyyy'];
-let bList1 = ['ayyyy'];
-let cList1 = ['ayyyy'];
-let dList1 = ['ayyyy'];
-let answer1 = ['a'];
-
-
+let qList1 = ['What does FBLA stand for?','What’s the minimum # of pretzel sticks we had to sell?','What’s the official colors for FBLA?','How many points do you need to become a member?','The first FBLA state is...'];
+let aList1 = ['Future Business Leaders of America','25','Blue and Gold','50','Iowa'];
+let bList1 = ['Future Business Leaders of Australia','30','Red, White, and Blue','40','New York'];
+let cList1 = ['Future Business in Los Angeles','15','Blue and Red','35','New Jersey'];
+let dList1 = ['Ford Business Learning Association','20','Green, Blue, and Red','25','Maryland'];
+let answer1 = ['a','b','a','d','a'];
 
 let qList2 = ['ayyyy'];
 let aList2 = ['ayyyy'];
 let bList2 = ['ayyyy'];
 let cList2 = ['ayyyy'];
 let dList2 = ['ayyyy'];
+let answer2 = ['a'];
