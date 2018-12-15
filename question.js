72b62b15762a8afcbd4b9d829bda8d220ea038d5
littlepann <45886128+littlepann@users.noreply.github.com>
2018-12-15 17:31:36 -0500

    questions for level 2

--- question.js
+++ question.js
@@ -6,9 +6,9 @@ let cList1 = ['Future Business in Los Angeles','15','Blue and Red','35','New Jer
 let dList1 = ['Ford Business Learning Association','20','Green, Blue, and Red','25','Maryland'];
 let answer1 = ['a','b','a','c','a'];
 
-let qList2 = ['ayyyy'];
-let aList2 = ['ayyyy'];
-let bList2 = ['ayyyy'];
-let cList2 = ['ayyyy'];
-let dList2 = ['ayyyy'];
-let answer2 = ['a'];
+let qList2 = ['Whats are the three words on the FBLA emblem?','Who is the CEO and President of FBLA?','FBLA and PBL are divided into___aministrative regions?','The FBLA-PBL national center is located where?','Who are the sponsors for FBLA?'];
+let aList2 = ['Future, Business, Leader','Eu Ro Wang','7','New York City, NewYork','Verizon'];
+let bList2 = ['Service, Education, and Progress','Stan Lee','9','Trenton, New Jersey','Walmart Stores'];
+let cList2 = ['Progresive, Leader, Manager','James Pan','3','Reston, Virgina','Geico'];
+let dList2 = ['Learned, Business, Skills','Sethu Senthil','5','Los Angeles, California','All of the Above'];
+let answer2 = ['b','a','d','c','d'];
