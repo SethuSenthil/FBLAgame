9df48e495a9632e93530748dd19def10e50f25a0
littlepann <45886128+littlepann@users.noreply.github.com>
2018-12-14 19:44:53 -0500

    new answers

--- question.js
+++ question.js
@@ -4,7 +4,7 @@ let aList1 = ['Future Business Leaders of America','25','Blue and Gold','50','Io
 let bList1 = ['Future Business Leaders of Australia','30','Red, White, and Blue','40','New York'];
 let cList1 = ['Future Business in Los Angeles','15','Blue and Red','35','New Jersey'];
 let dList1 = ['Ford Business Learning Association','20','Green, Blue, and Red','25','Maryland'];
-let answer1 = ['a','b','a','d','a'];
+let answer1 = ['a','b','a','c','a'];
 
 let qList2 = ['ayyyy'];
 let aList2 = ['ayyyy'];
