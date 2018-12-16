c08ad5961002cb2dd39016c87c900d1a0b2c05da
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-12-15 21:33:19 -0500

    Game start UI, escape key exit cmd, Game menue, new bg music

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   create: create,
   update: update })
 
-// Declare shared variables at the top so all methods can access them
+  // Declare shared variables at the top so all methods can access them
 let scoreText
 let platforms
 let diamonds
@@ -38,8 +38,21 @@ function preload () {
 
 
 }
+function gofull() {
+
+  if (game.scale.isFullScreen)
+  {
+      game.scale.stopFullScreen();
+  }
+  else
+  {
+      game.scale.startFullScreen(false);
+  }
+
+}
 
 function create () {
+
 
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
@@ -48,6 +61,15 @@ function create () {
 
   game.world.setBounds(0, 0, 5000, 300);
 
+  game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+
+  // Keep original size
+  // game.scale.fullScreenScaleMode = Phaser.ScaleManager.NO_SCALE;
+
+  // Maintain aspect ratio
+  // game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+  game.input.onDown.add(gofull, this);
   platforms = game.add.group();
   hurt = game.add.group();
   oof = game.add.group();
@@ -61,7 +83,7 @@ function create () {
   qPulse = game.add.audio('qSound');
   coinCollect = game.add.audio('coin');
   music = game.add.audio('bgMusic');
-  music.volume = 0.1;
+  music.volume = 0.03;
   music.play();
 
 
@@ -162,61 +184,44 @@ function create () {
     //  We need to enable physics on the player
   game.physics.arcade.enable(enemy)
 
-    //  Player physics properties. Give the little guy a slight bounce.
     enemy.body.bounce.y = 0.2
     enemy.body.gravity.y = 700
     enemy.body.collideWorldBounds = true
 
-    //  Our two animations, walking left and right.
   enemy.animations.add('left', [0, 1], 10, true)
   enemy.animations.add('right', [2, 3], 10, true)
 
-    //  Finally some diamonds to collect
   diamonds = game.add.group();
   qblock = game.add.group();
 
-
-    //  Enable physics for any object that is created in enemy group
   diamonds.enableBody = true
   qblock.enableBody = true
 
-
-
-    //  Create 12 diamonds evenly spaced apart
-
   for (var i = 1; i < 20; i++) {
     let loopQ = diamonds.create( i * 140, 0, 'diamond')
-
-      //  Drop em from the sky and bounce a bit
       loopQ.body.gravity.y = 1000
     loopQ.body.bounce.y = 0.2 + Math.random() * 0.1
   }
 
   for (var i = 1; i < 5; i++) {
     let loopQ = qblock.create(i * 300, 0, 'question')
-
-      //  Drop em from the sky and bounce a bit
       loopQ.body.gravity.y = 1000
     loopQ.body.bounce.y = 0.2 + Math.random() * 0.1
   }
 
-    //  Create the score text
   scoreText = game.add.bitmapText(16, 16, 'pixyfont', { fontSize: '32px' });
   scoreText.fixedToCamera = true;
 
 
-    //  And bootstrap our controls
   cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
 
-    //  We want the player to stop when not moving
   player.body.velocity.x = 0
   enemy.body.velocity.x = 0
 
 
-    //  Setup collisions for the player, diamonds, and our platforms
   game.physics.arcade.collide(player, platforms)
   game.physics.arcade.collide(player, oof)
   game.physics.arcade.collide(oof, player)
@@ -227,7 +232,6 @@ function update () {
 
 
 
-    //  Call callectionDiamond() if player overlaps with a diamond
   game.physics.arcade.overlap(player, diamonds, collectDiamond, null)
 
   game.physics.arcade.overlap(player, oof, die, null)
@@ -236,7 +240,6 @@ function update () {
 
 
 
-    // Configure the controls!
   if (cursors.left.isDown) {
     player.body.velocity.x = -150
     player.animations.play('left')
@@ -245,15 +248,12 @@ function update () {
 
     player.animations.play('right')
   } else {
-    // If no movement keys are pressed, stop the player
     player.animations.stop()
   }
 
-    //  enemy allows the player to jump!
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -400
   }
-    // Show an alert modal when score reaches 120
   if (score === 120) {
     alert('You win!')
     score = 0
@@ -280,6 +280,7 @@ function question(player, qblock){
 function pauser() {
     console.log('paused');
 }
-function die() {
+function die(player, oof) {
+  player.kill();
     alert('you died');
 }
