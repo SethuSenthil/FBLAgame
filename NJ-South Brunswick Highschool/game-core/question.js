af630d71b3efe74ee40baebfa57b1d04bc973002
littlepann <45886128+littlepann@users.noreply.github.com>
2019-01-13 20:52:40 -0500

    delete buttons

--- NJ-South Brunswick Highschool/game-core/question.js
+++ NJ-South Brunswick Highschool/game-core/question.js
@@ -9,7 +9,7 @@ let aList1 = ['Future Business Leaders of America','3000,000','Blue and Gold','5
 let bList1 = ['Future Business Leaders of Australia','250,000','Red, White, and Blue','10','New York'];
 let cList1 = ['Future Business in Los Angeles','150,000','Blue and Red','9','New Jersey'];
 let dList1 = ['Ford Business Learning Association','200,000','Green, Blue, and Red','7','Iowa'];
-let answer1 = ['a','b','a','c','d'];
+let answer1 = ['a','b','a','d','d'];
 
 let qList2 = ['Whats are the three words on the FBLA emblem?','Who is the CEO and President of FBLA?','FBLA and PBL are divided into___aministrative regions?','The FBLA-PBL national center is located where?','How many votes would a chapter of 99 members have at the National Leadership Conference?'];
 let aList2 = ['Future, Business, Leader','Eu Ro Wang','7','New York City, NewYork','3'];
