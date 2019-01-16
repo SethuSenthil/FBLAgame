0112a6ef0a63e955d868daf3248c3a689cdac02e
Sethu Senthil <sethunarayansenthil@gmail.com>
2019-01-15 20:23:19 -0500

    open in chrome tab auto

--- CatRush.sh
+++ CatRush.sh
@@ -6,6 +6,4 @@ echo ""
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-open $parent_path/NJ-South\ Brunswick\ Highschool/desktop-wrapper/dist/mac/FBLA.app
-
 sh $parent_path/NJ-South\ Brunswick\ Highschool/game-core/nodeRun.sh &  echo "click ctrl c to quit"
--- NJ-South Brunswick Highschool/game-core/server.js
+++ NJ-South Brunswick Highschool/game-core/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const opn = require('opn');
 const PORT =  process.env.PORT || 2343;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 app.use(express.static(__dirname));
 
-app.listen(PORT, () => console.log(`Server Running...`))
+app.listen(PORT, () => console.log(`Server Running...`));
+opn('localhost:2343', {app: ['google chrome', '--incognito']});
